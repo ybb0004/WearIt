ce798c81e8715efbde6513a11b6ee4e5fd086f32
ybb0004 <ybb0004@alu.medac.es>
2025-03-10 12:23:17 +0100

    Commit Activity de Inicio y fragment inicio

---
+++ WearIt/app/src/main/java/com/example/wearit/W01_0000_Activity_Inicio_App.java
@@ -0,0 +1,14 @@
+package com.example.wearit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class W01_0000_Activity_Inicio_App extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_w010000_inicio_app);
+    }
+}
