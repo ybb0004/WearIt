f1957d94a535d7c7bd2651ce5054eb3116079157
ybb0004 <ybb0004@alu.medac.es>
2025-03-11 13:09:05 +0100

    Reparado error activitys
    Error que no presentaba los activitis

---
+++ WearIt/app/src/main/java/com/example/wearit/activity_w020000_home_app.java
@@ -0,0 +1,14 @@
+package com.example.wearit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_w020000_home_app extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_w020000_home_app);
+    }
+}
