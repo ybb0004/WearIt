c5d940fadcc75cb5b78f8f0a3d8f05dcbe3c40a2
ybb0004 <ybb0004@alu.medac.es>
2025-03-11 14:35:45 +0100

    Menu y Activity Home
    Menu y Activity Home creadas

--- WearIt/app/src/main/java/com/example/wearit/activity_w020000_home_app.java
+++ WearIt/app/src/main/java/com/example/wearit/activity_w020000_home_app.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class activity_w020000_home_app extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class activity_w020000_home_app extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_w020000_home_app);
+
     }
+
+
+
+
 }
