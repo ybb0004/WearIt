b40cd1b3917a597951657b8713c6d6d758000d8a
marcossanchezag <msa0036@alu.medac.es>
2025-03-11 12:52:20 +0100

    commit implentacion dataTime firebase

--- WearIt/app/build.gradle.kts
+++ WearIt/app/build.gradle.kts
@@ -34,6 +34,8 @@ dependencies {
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
     implementation("com.google.firebase:firebase-analytics")
+
+    implementation("com.google.firebase:firebase-database-ktx")
     //
 
     implementation("androidx.appcompat:appcompat:1.6.1")
@@ -41,6 +43,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment:2.8.8")
     implementation("androidx.navigation:navigation-ui:2.8.8")
+    implementation("com.google.firebase:firebase-database:21.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
