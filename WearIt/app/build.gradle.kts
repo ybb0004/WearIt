a5fa17aec4eefa74b83dda32002d7ad40584a309
ybb0004 <ybb0004@alu.medac.es>
2025-03-12 11:13:17 +0100

    Pantalla home v1
    creada pantalla home

--- WearIt/app/build.gradle.kts
+++ WearIt/app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
+
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment:2.8.8")
     implementation("androidx.navigation:navigation-ui:2.8.8")
