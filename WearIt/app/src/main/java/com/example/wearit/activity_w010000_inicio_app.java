8f590fb389a63a95c093dd973dfdd6941495b24e
ybb0004 <ybb0004@alu.medac.es>
2025-06-12 11:30:34 +0200

    cambio color barra

--- WearIt/app/src/main/java/com/example/wearit/activity_w010000_inicio_app.java
+++ WearIt/app/src/main/java/com/example/wearit/activity_w010000_inicio_app.java
@@ -1,7 +1,11 @@
 package com.example.wearit;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class activity_w010000_inicio_app extends AppCompatActivity {
 
@@ -9,11 +13,19 @@ public class activity_w010000_inicio_app extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_w010000_inicio_app);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        getWindow().setStatusBarColor(ContextCompat.getColor(getApplicationContext(),R.color.color_naranja_claro));
+
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.inicoRegistro, new fragment_w01_0001_inicio_registro())
                     .commit();
+        }else {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_w02_0001_home, new fragment_w02_0001_home())
+                    .commit();
         }
+
     }
 }
